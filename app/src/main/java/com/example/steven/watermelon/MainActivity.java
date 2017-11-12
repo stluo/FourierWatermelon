5be2e5e408739045c86afb813062da5da9ad1ee7
stluo <swluo@uwaterloo.ca>
2017-11-11 18:31:12 -0600

    intergrated and building

--- app/src/main/java/com/example/steven/watermelon/MainActivity.java
+++ app/src/main/java/com/example/steven/watermelon/MainActivity.java
@@ -1,5 +1,31 @@
 package com.example.steven.watermelon;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+import android.Manifest;
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.pm.PackageManager;
+import android.media.AudioFormat;
+import android.media.AudioRecord;
+import android.media.MediaRecorder;
+import android.os.Bundle;
+import android.os.Environment;
+import android.support.annotation.NonNull;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.Button;
+import android.support.v4.content.ContextCompat;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import android.media.AudioFormat;
+import android.media.AudioRecord;
 import android.media.MediaPlayer;
 import android.media.MediaRecorder;
 import android.support.v7.app.AppCompatActivity;
@@ -8,6 +34,7 @@ import android.app.Activity;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.view.MotionEvent;
 import android.widget.Button;
 import android.view.View;
 import android.view.View.OnClickListener;
@@ -50,91 +77,270 @@ import java.util.List;
 import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity {
+    private static final int RECORDER_BPP = 16;
+    private static final String AUDIO_RECORDER_FILE_EXT_WAV = ".wav";
+    private static final String AUDIO_RECORDER_FOLDER = "AudioRecorder";
+    private static final String AUDIO_RECORDER_TEMP_FILE = "record_temp.raw";
+    private static final int RECORDER_SAMPLERATE = 44100;
+    private static final int RECORDER_CHANNELS = AudioFormat.CHANNEL_IN_STEREO;
+    private static final int RECORDER_AUDIO_ENCODING = AudioFormat.ENCODING_PCM_16BIT;
+    short[] audioData;
 
-    private Camera mCamera;
-    private TextureView mPreview;
-    private MediaRecorder mMediaRecorder;
-    private File mOutputFile;
-
+    private AudioRecord recorder = null;
+    private int bufferSize = 0;
+    private Thread recordingThread = null;
     private boolean isRecording = false;
-    private static final String TAG = "Recorder";
-    private Button captureButton;
+    Complex[] fftTempArray;
+    Complex[] fftArray;
+    int[] bufferData;
+    int bytesRecorded;
+
+    double[] absNormalizedSignal;
+
+    private static final int ALL= 200;
+    private boolean permissionToRecordAccepted = false;
+    private String [] permissions = {Manifest.permission.RECORD_AUDIO, Manifest.permission.WRITE_EXTERNAL_STORAGE, Manifest.permission.READ_EXTERNAL_STORAGE};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        this.requestPermissions(permissions, REQUEST_RECORD_AUDIO_PERMISSION);
+        setButtonHandlers();
 
-        captureButton = (Button) findViewById(R.id.record);
-        final TextView sweetness = findViewById(R.id.textNumber);
+        bufferSize = AudioRecord.getMinBufferSize
+                (RECORDER_SAMPLERATE,RECORDER_CHANNELS,RECORDER_AUDIO_ENCODING)*3;
 
-        final Button button = findViewById(R.id.record);
-        button.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-//                Intent browserIntent =
-//                        new Intent(Intent.ACTION_VIEW, Uri.parse("http://www.facebook.com"));
-//                startActivity(browserIntent);
-                button.setVisibility(View.INVISIBLE);
-                sweetness.setText("120%");
-                sweetness.setVisibility(View.VISIBLE);
-            }
-        });
+        audioData = new short [bufferSize]; //short array that pcm data is put into.
+
+        this.requestPermissions(permissions,ALL);
+
     }
 
-    // Requesting permission to RECORD_AUDIO
-    private static final int REQUEST_RECORD_AUDIO_PERMISSION = 200;
-
-    private void onRecord(boolean start) {
-        if (start) {
-            startRecording();
-        } else {
-            stopRecording();
-        }
-    }
-
-    private MediaRecorder mRecorder = null;
-    private static final String LOG_TAG = "AudioRecordTest";
-    private static String mFileName = null;
-
-    private void startRecording() {
-        mRecorder = new MediaRecorder();
-        mRecorder.setAudioSource(MediaRecorder.AudioSource.MIC);
-        mRecorder.setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP);
-        mRecorder.setOutputFile(mFileName);
-        mRecorder.setAudioEncoder(MediaRecorder.AudioEncoder.AMR_NB);
-
-        try {
-            mRecorder.prepare();
-        } catch (IOException e) {
-            Log.e(LOG_TAG, "prepare() failed");
-        }
-
-        mRecorder.start();
-    }
-
-    private void stopRecording() {
-        mRecorder.stop();
-        mRecorder.release();
-        mRecorder = null;
-    }
 
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-        switch (requestCode){
-            case REQUEST_RECORD_AUDIO_PERMISSION:
-                permissionToRecordAccepted  = grantResults[0] == PackageManager.PERMISSION_GRANTED;
-                break;
-        }
-        if (!permissionToRecordAccepted ) finish();
+
+        permissionToRecordAccepted  = grantResults[0] == PackageManager.PERMISSION_GRANTED;
+
 
     }
 
-    private boolean permissionToRecordAccepted = false;
-    private String [] permissions = {Manifest.permission.RECORD_AUDIO};
+    private void setButtonHandlers() {
+        ((Button)findViewById(R.id.record)).setOnTouchListener(btnClick);
+    }
 
+    private View.OnTouchListener btnClick = new View.OnTouchListener() {
+        public boolean onTouch(View view, MotionEvent motionEvent) {
+            switch ( motionEvent.getAction() ) {
+                case MotionEvent.ACTION_DOWN: {
+                    startRecording();
+
+                    break;
+                }
+
+                case MotionEvent.ACTION_UP: {
+                    stopRecording();
+
+                    Button b = findViewById(R.id.record);
+                    b.setVisibility(view.INVISIBLE);
+
+                   TextView view1 = findViewById(R.id.textNumber);
+                   view1.setVisibility(view.VISIBLE);
+                   view1.setText(String.valueOf(absNormalizedSignal[0]));
+
+                    break;
+                }
+
+            }
+            return true;
+        }
+    };
+
+    private String getFilename(){
+        String filepath = Environment.getExternalStorageDirectory().getPath();
+        File file = new File(filepath,AUDIO_RECORDER_FOLDER);
+
+        if(!file.exists()){
+            file.mkdirs();
+        }
+
+        return (file.getAbsolutePath() + "/" + System.currentTimeMillis() + AUDIO_RECORDER_FILE_EXT_WAV);
+    }
+
+
+    public double[] calculateFFT(byte[] signal)
+    {
+        final int mNumberOfFFTPoints =1024;
+        double mMaxFFTSample;
+        double mPeakPos;
+
+        double temp;
+        Complex[] y;
+        Complex[] complexSignal = new Complex[mNumberOfFFTPoints];
+        double[] absSignal = new double[mNumberOfFFTPoints/2];
+
+        for(int i = 0; i < mNumberOfFFTPoints; i++){
+            temp = (double)((signal[2*i] & 0xFF) | (signal[2*i+1] << 8)) / 32768.0F;
+            complexSignal[i] = new Complex(temp,0.0);
+        }
+
+        y = FFT.fft(complexSignal); // --> Here I use FFT class
+
+        mMaxFFTSample = 0.0;
+        mPeakPos = 0;
+        for(int i = 0; i < (mNumberOfFFTPoints/2); i++)
+        {
+            absSignal[i] = Math.sqrt(Math.pow(y[i].re(), 2) + Math.pow(y[i].im(), 2));
+            if(absSignal[i] > mMaxFFTSample)
+            {
+                mMaxFFTSample = absSignal[i];
+                mPeakPos = i;
+            }
+        }
+
+        return absSignal;
+    }
+
+
+    private String getTempFilename(){
+        String filepath = Environment.getExternalStorageDirectory().getPath();
+        File file = new File(filepath,AUDIO_RECORDER_FOLDER);
+
+        if(!file.exists()){
+            file.mkdirs();
+        }
+
+        File tempFile = new File(filepath,AUDIO_RECORDER_TEMP_FILE);
+
+        if(tempFile.exists())
+            tempFile.delete();
+
+        return (file.getAbsolutePath() + "/" + AUDIO_RECORDER_TEMP_FILE);
+    }
+
+    private void startRecording(){
+        recorder = new AudioRecord(MediaRecorder.AudioSource.MIC,
+                RECORDER_SAMPLERATE, RECORDER_CHANNELS,RECORDER_AUDIO_ENCODING, bufferSize);
+
+        recorder.startRecording();
+
+        isRecording = true;
+
+        recordingThread = new Thread(new Runnable() {
+
+            public void run() {
+                writeAudioDataToFile();
+            }
+        },"AudioRecorder Thread");
+
+        recordingThread.start();
+    }
+
+    private void writeAudioDataToFile(){
+
+        byte data[] = new byte[bufferSize];
+        String filename = getTempFilename();
+        FileOutputStream os = null;
+
+        try {
+            os = new FileOutputStream(filename);
+        } catch (FileNotFoundException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+
+        int read = 0;
+        if(null != os){
+            while(isRecording){
+                read = recorder.read(data, 0, bufferSize);
+                if(read > 0){
+                    absNormalizedSignal = calculateFFT(data);
+
+
+
+                }
+
+                if(AudioRecord.ERROR_INVALID_OPERATION != read){
+                    try {
+                        os.write(data);
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+
+            try {
+                os.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private void stopRecording(){
+        if(null != recorder){
+            isRecording = false;
+
+            recorder.stop();
+            recorder.release();
+
+            recorder = null;
+            recordingThread = null;
+        }
+
+        copyWaveFile(getTempFilename(),getFilename());
+        // deleteTempFile();
+    }
+
+    private void deleteTempFile() {
+        File file = new File(getTempFilename());
+        file.delete();
+    }
+
+    private void copyWaveFile(String inFilename,String outFilename){
+        FileInputStream in = null;
+        FileOutputStream out = null;
+        long totalAudioLen = 0;
+        long totalDataLen = totalAudioLen + 36;
+        long longSampleRate = RECORDER_SAMPLERATE;
+        int channels = 2;
+        long byteRate = RECORDER_BPP * RECORDER_SAMPLERATE * channels/8;
+
+        byte[] data = new byte[bufferSize];
+
+        try {
+            in = new FileInputStream(inFilename);
+            out = new FileOutputStream(outFilename);
+            totalAudioLen = in.getChannel().size();
+            totalDataLen = totalAudioLen + 36;
+
+            //AppLog.logString("File size: " + totalDataLen);
+
+            WriteWaveFileHeader(out, totalAudioLen, totalDataLen,
+                    longSampleRate, channels, byteRate);
+
+            while(in.read(data) != -1){
+                out.write(data);
+            }
+
+            in.close();
+            out.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void WriteWaveFileHeader(
+            FileOutputStream out, long totalAudioLen,
+            long totalDataLen, long longSampleRate, int channels,
+            long byteRate) throws IOException {
+        //another code
+
+    }
 
 
 
